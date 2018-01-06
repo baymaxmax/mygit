1da7e878841c2f5a10c5e11535d5d5115e1b5e7d
baymaxmax <yuncongbai@sina.com>
2018-01-06 20:20:55 +0800

    letao2018/1/6 8pm

---
+++ letao/js/index.js
@@ -0,0 +1,8 @@
+
+window.onload = function(){
+    // 给banner 和 nav 加高度
+    //  $(".main .banner").css("height",$(".main .banner img").height());
+    //  $(".main .nav").height($(".main .nav img")*2);
+    console.log($("html").width());
+}
+   
